a1055f55da359d1631aa92c2fad77245281e6b43
kevin <liujiezhang@bupt.edu.cn>
2016-11-25 10:52:59 +0800

    add mysfunc

---
+++ spiders/myfunc.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+"""公共函数
+@version: 1.0
+@author: kevin
+@license: Apache Licence
+@contact: liujiezhang@bupt.edu.cn
+@site:
+@software: PyCharm Community Edition
+@file: myfunc.py
+@time: 16/11/25 上午10:45
+"""
+import urllib
+import urllib2
+
+def getHtml(url,post_data=''):
+    '''
+    获取url对应Html内容
+    :param url: 请求url
+    :param post_data: post参数
+    :return: html页面
+    '''
+    if post_data and isinstance(post_data,dict):
+        data = urllib.urlencode(post_data)
+        req = urllib2.Request(url,post_data=data)
+    else:
+        req = urllib2.Request(url)
+    try:
+        res = urllib2.urlopen(req).read()
+        return res
+    except Exception,e:
+        print( Exception,":",e)

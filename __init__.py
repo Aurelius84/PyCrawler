e04ddf7a69a7ec95f18c36f22b33adb1c5dea862
kevin <liujiezhang@bupt.edu.cn>
2016-11-26 20:52:08 +0800

    add init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+"""
+@version: 1.0
+@author: kevin
+@license: Apache Licence 
+@contact: liujiezhang@bupt.edu.cn
+@site: 
+@software: PyCharm Community Edition
+@file: __init__.py
+@time: 16/11/26 下午8:48
+"""

17885835171858b4ed4a316d9a9e58b22aa4b746
kevin <liujiezhang@bupt.edu.cn>
2016-11-29 21:52:38 +0800

    add Mayi vpn

--- spiders/myfunc.py
+++ spiders/myfunc.py
@@ -13,6 +13,8 @@
 import urllib
 import urllib2
 from tornado_fetcher import Fetcher
+import hashlib
+import time
 
 def getHtml(url,post_data=''):
     '''
@@ -46,4 +48,38 @@ def getHtmlFromJs(url):
           async=False
           )
     return fetcher.phantomjs_fetch(url)
+
+def getHtmlByVPN(url):
+    '''
+    蚂蚁代理获取页面
+    :param url:
+    :return:
+    '''
+    #请替换appkey和secret
+    appkey = "151161671"
+    secret = "f7d28632cdf6ce18a2cb3672376d166d"
+    paramMap = {
+        "app_key": appkey,
+        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S")  #如果你的程序在国外，请进行时区处理
+    }
+    #排序
+    keys = paramMap.keys()
+    keys.sort()
+    codes= "%s%s%s" % (secret,str().join('%s%s' % (key, paramMap[key]) for key in keys),secret)
+    #计算签名
+    sign = hashlib.md5(codes).hexdigest().upper()
+    paramMap["sign"] = sign
+    #拼装请求头Proxy-Authorization的值
+    keys = paramMap.keys()
+    authHeader = "MYH-AUTH-MD5 " + str('&').join('%s=%s' % (key, paramMap[key]) for key in keys)
+    print authHeader
+    #接下来使用蚂蚁动态代理进行访问
+    proxy_handler = urllib2.ProxyHandler({"http" : '123.56.92.151:8123'})
+    opener = urllib2.build_opener(proxy_handler)
+    request = urllib2.Request(url)
+    # 将authHeader放入请求头中即可,注意authHeader必须在每次请求时都重新计算，要不然会因为时间误差而认证失败
+    request.add_header('Proxy-Authorization', authHeader)
+    response = opener.open(request)
+    return response.read()
+
 

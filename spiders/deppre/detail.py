ed403aad7dc5f3bee073cfc26d6467c65e9fa8d6
Marcovaldong <dsq0720@163.com>
2016-12-01 23:07:30 +0800

    add deppre/detail.py

---
+++ spiders/deppre/detail.py
@@ -0,0 +1,184 @@
+# -*- coding:utf-8 -*-
+
+"""
+@version: 1.0
+@author: marcovaldo
+@license: Apache Licence
+@contact: dsq0720@163.com
+@site:
+@software: PyCharm Community Edition
+@file: __init__.py.py
+@time: 16/12/1 下午14:09
+"""
+
+import sys
+sys.path.append("..")
+reload(sys)
+sys.setdefaultencoding('utf8')
+from myfunc import *
+from scrapy.http import HtmlResponse
+from collections import defaultdict
+import itertools
+import time
+import re
+import requests
+
+def goodsOutline(url):
+    '''
+    获取三级目录详情
+    :param url: 网站主页链接
+    :return: [{'url':'','first_grade':'','second_grade': '', 'third_grade': ''},...{}]
+    '''
+    # 存储最后的三级列表页面
+    outline_data = []
+    # 解析页面
+    body = getHtml(url).encode('utf-8')
+    html = HtmlResponse(url=url, body=str(body))
+    # soup = BeautifulSoup(body, 'lxml')      # xpath不好使就用beautifulsoup
+    # 抓取一级目录名及链接，这里只要以下内容：
+    # 这里不要第零个（工控自动化）、第二个（五金工具）
+    first_grade_name = ['工控自动化', '电工电气', '五金工具', '仪器仪表', '气动液压泵阀', '通风换热', '动力传动', '安防劳保']
+    num = [1, 3, 4, 5, 6, 7]    # //*[@id="c' + str(i) + '"]/div[1]/div[1]/dt/a/text()
+
+    for i in num:
+        print('一级目录：' + first_grade_name[i])
+        second_grade_name = html.xpath('//*[@id="c' + str(i) + '"]/div[1]/div/dt/a/text()').extract()
+        print len(second_grade_name)
+        for j in range(0, len(second_grade_name)):
+            print(' 二级目录：' + second_grade_name[j])
+            third_grade_name = html.xpath('//*[@id="c' + str(i) + '"]/div[1]/div['+ str(j+1) + ']/dd/a/span/text()').extract()
+            third_grade_url = html.xpath('//*[@id="c' + str(i) + '"]/div[1]/div[' + str(j+1) + ']/dd/a/@href').extract()
+            print len(third_grade_name)
+            for k in range(len(third_grade_name)):
+                url_data = defaultdict()
+                url_data['url'] = 'http://www.deppre.cn/' + third_grade_url[k]
+                url_data['third_grade'] = third_grade_name[k]
+                print('     三级目录：' + url_data['third_grade'])
+                url_data['second_grade'] = second_grade_name[j]
+                url_data['first_grade'] = first_grade_name[i]
+                url_data['created'] = int(time.time())
+                url_data['updated'] = int(time.time())
+                outline_data.append(url_data)
+    print len(outline_data)
+    return outline_data
+
+def goodsUrlList(home_url):
+    '''
+    根据三级目录商品首页获取所有详情页url
+    :param home_url: http://www.sssmro.com//category.php?id=1137&price_min=&price_max=
+    :return:url列表
+    '''
+    # 该网站不用加条件遍历所有情况就能拿到所有产品的url
+    print home_url
+    body = getHtml(home_url)
+    html = HtmlResponse(url=home_url, body=str(body))
+    url_list = []
+    # 拿到该三级目录下一共有多少种产品
+    num = int(html.xpath('//*[@id="pinpai"]/a[1]/text()').extract()[0][3:-1])
+    for i in range(1, num + 1):
+        if not html.xpath('/html/body/div[5]/form[1]/ul/li['+str(i)+']/p[2]/span[1]/text()').extract()[0] == '询价':
+            url_list.append('http://www.deppre.cn/' + html.xpath('/html/body/div[5]/form[1]/ul/li['+str(i)+']/a/@href').extract()[0])
+            print('http://www.deppre.cn/' + html.xpath('/html/body/div[5]/form[1]/ul/li['+str(i)+']/a/@href').extract()[0])
+    print len(url_list)
+    return url_list
+
+def goodsDetail(detail_url):
+    '''
+    利用xpath解析关键字段
+    :param detail_url: 详情页url
+    :return: 因为每个详情页面可能会产生多条数据，所以返回值是一个以dict为元素的list，其中每一个dict是一条数据
+    '''
+    # 解析网页
+    body = getHtml(detail_url).encode('utf-8')
+    html = HtmlResponse(url=detail_url, body=str(body))
+    print '拿到数据，正在解析...'
+    # 名称
+    name = '原装进口_' + html.selector.xpath('//*[@id="pic_Img"]/@alt').extract()[0]
+    # 本网站下商品会有多个规格，需一次抓取多个型号和价格
+    # 价格，所有的价格都在这个list中
+    price_list = html.xpath('//*[@id="ECS_FORMBUY"]/div[1]/table/tr/td/span/text()').extract()
+    # 型号和货期一块儿拿到
+    # 详情，本网站下的商品只有p标签没有table
+    # print html.xpath('//*[@id="cc1"]/div/div[2]/p[1]/span[2]/text()').extract()[0]
+    # print html.xpath('//*[@id="cc1"]/div/div[2]/p[1]/span[2]/a/strong/text()').extract()[0]
+    detailInfo = html.xpath('//*[@id="cc1"]/div/div[2]/p/span/text()').extract()[1:]
+    # print '呵呵'.find('：')
+    detailInfo = handle(detailInfo)   # 将p标签里的内容封装成符合格式的p标签
+    print detailInfo
+    exit()
+    # 图片，本网站下的商品都只有一张图片
+    pic = 'http://www.deppre.cn/' + html.xpath('//*[@id="pic_Img"]/@src').extract()[0]
+    goodslist = []
+    for i in range(len(price_list)):
+        goodslist.append(defaultdict())
+        goodslist[i]['price'] = float(price_list[i][1:])
+        type_lack_period = html.xpath('//*[@id="ECS_FORMBUY"]/div[1]/table/tr[' + str(3+i) + ']/td/text()').extract()
+        goodslist[i]['type'] = type_lack_period[0]
+        goodslist[i]['source_url'] = detail_url
+        goodslist[i]['name'] = name
+        goodslist[i]['detail'] = detailInfo
+        goodslist[i]['pics'] = pic
+        goodslist[i]['storage'] = ''
+        goodslist[i]['lack_period'] = type_lack_period.pop()
+        goodslist[i]['created'] = int(time.time())
+        goodslist[i]['updated'] = int(time.time())
+        goodslist[i]['first_grade'] = ''
+        goodslist[i]['second_grade'] = ''
+        goodslist[i]['third_grade'] = ''
+    return goodslist
+
+def handle(pLabel):
+    """
+    # 处理p标签
+    :param pLabel: p标签中包含的内容
+    :return: 符合要求的p标签语句
+    """
+    for s in pLabel:
+        print s
+    label = '<p>产品介绍：</p>\n'
+    for i in range(len(pLabel)):
+        if pLabel[i].encode('utf-8').find('：') == -1:
+            label += '<p>' + pLabel[i].encode('utf-8').replace('\n', '') + '</p>\n'
+        elif not (pLabel[i-1].encode('utf-8').find('：') == -1):
+            label += '<p>' + pLabel[i].encode('utf-8').replace('\n', '') + ','
+        else:
+            label += pLabel[i].encode('utf-8').replace('\n', '') + ','
+    label = unicode(label, 'utf-8')
+    # 格式
+    font = '<style>.default p{padding:0;margin:0;font-family:微软雅黑;' \
+           'font-size:18px;' \
+           'line-height:28px;color:#333;' \
+           'width:780px;text-indent:-5rem;margin-left:6.2rem}</style>'
+    font = unicode(font, 'utf-8')
+    return label + font
+
+
+
+if __name__ == '__main__':
+
+    # 测试函数goodsDetail(detail_url)
+
+    url = 'http://www.deppre.cn/goods-750.html'
+    detail = goodsDetail(url)
+    # print detail['source_url']
+    # print detail['name']
+    # print detail['price']
+    # print detail['type']
+    # print detail['detail']
+    # print detail['pics']
+    # print detail['storage']
+    # print detail['lack_period']
+
+    # 测试goodsOutline()
+    # url = 'http://www.deppre.cn/'
+    # goodsOutline(url)
+
+
+
+    # 测试函数goodsOutline(url)
+    # url = 'http://mro.abiz.com'
+    # goodsOutline(url)
+
+    # 测试函数goodsUrlList(home_url)
+    # url = 'http://www.deppre.cn/category-125-b0.html'
+    # goodsUrlList(url)
---
+++ spiders/deppre/fetch.py
@@ -0,0 +1,207 @@
+# -*- coding:utf-8 -*-
+
+"""
+抓取主逻辑,分三个进程:解析三级目录,抓取种子,解析详情
+
+@version: 1.0
+@author: kevin
+@license: Apache Licence
+@contact: liujiezhang@bupt.edu.cn
+@site:
+@software: PyCharm Community Edition
+@file: fetch.py
+@time: 16/11/26 下午5:21
+"""
+import sys
+import getopt
+# from dataBase.mysql import M
+from mysql import M
+from detail import *
+import time
+import random
+# from dataMin.ETL import ETL
+
+def parseOutline():
+    '''
+    解析类目函数入口
+    :return: True or False
+    '''
+    url = 'http://mro.abiz.com'
+    try:
+        # 实例化 表
+        table_outline = M('abiz', 'abiz_outline')
+    except Exception, e:
+        print(Exception, ":", e)
+        return False
+    # 抓取三级类目信息
+    outline_data = goodsOutline(url)
+    print(len(outline_data))
+    if not outline_data:
+        print('Failed to parse outline, data is empty.')
+        return False
+    table_outline.insertAll(outline_data)
+    print('Successfully parse outline data.')
+    # 关闭数据库连接
+    table_outline.close()
+    return True
+
+def parseSeedUrl():
+    '''
+    抓取种子入口
+    :return:
+    '''
+    # 实例化 outline表
+    table_name = 'abiz_outline'
+    table_seed_name = 'abiz_url'
+    table_outline = M('abiz',table_name)
+    table_seed = M('abiz',table_seed_name)
+    # 查询所有url
+    sql = 'select * from {0} order by id'.format(table_name)
+    table_outline.cursor.execute(sql)
+    outline_data = table_outline.cursor.fetchall()
+    for data in outline_data:
+        # 获取种子url
+        seed_urls = goodsUrlList(data['url'])
+        if not seed_urls: # 为空,跳过
+            continue
+        # 插入数据列表
+        insert_data = []
+        for seed_url in seed_urls:
+            # 先查询mysql中是否已经存在
+            sql = "select id from {0} where url='{1}' order by id limit 1".format(table_seed_name,seed_url)
+            is_exist = table_seed.cursor.execute(sql)
+            if is_exist:
+                continue
+            seed_data = defaultdict()
+            seed_data['url'] = seed_url
+            seed_data['source_url'] = data['url']
+            seed_data['first_grade'] = data['first_grade']
+            seed_data['second_grade'] = data['second_grade']
+            seed_data['third_grade'] = data['third_grade']
+            seed_data['created'] = int(time.time())
+            seed_data['updated'] = int(time.time())
+            insert_data.append(seed_data)
+        # 插入数据库
+        table_seed.insertAll(insert_data)
+    # 关闭数据库
+    table_seed.close()
+    table_outline.close()
+
+def parseDetail():
+    '''
+    解析详情函数入口
+    :return:
+    '''
+    table_seed_name = 'abiz_url'
+    table_gov_name = 'abiz_gov'
+    table_seed = M('abiz',table_seed_name)
+    table_gov = M('abiz',table_gov_name)
+    # 查询未入库种子
+    sql = "select a.id,a.url from {0} a where a.url not in (select source_url from {1} order by id)  order by id".format(table_seed_name,table_gov_name)
+    table_seed.cursor.execute(sql)
+    seed_urls = table_seed.cursor.fetchall()
+    for seed in seed_urls:
+        time.sleep(random.randint(1, 3))
+        print seed['url']
+        flag = True
+        start = time.time()
+        while(flag):
+            try:
+                detail = goodsDetail(seed['url'])
+                flag = False
+            except:
+                flag = True
+                time.sleep(random.randint(0, 1))
+        print detail
+        end = time.time()
+        print('抓取解析本条数据耗时 %f sec.' % (end- start))
+        # 插入数据库
+        table_gov.insertOne(detail)
+    # 关闭数据库连接
+    table_seed.close()
+    table_gov.close()
+
+def etl():
+    site = 'abiz'
+    db_name = 'abiz'
+    # gov表
+    gov_name = site + '_gov'
+    table_gov = M(db_name,gov_name)
+    # 每次处理量
+    iter_count = 500
+    sql = 'select * from {0} where is_contrast=0 order by id limit {1}'.format(gov_name,iter_count)
+    n = table_gov.cursor.execute(sql)
+    etl_func = ETL(db_name=db_name,site_name=site)
+    datas = table_gov.cursor.fetchall()
+    while datas:
+        print(list(datas)[0]['id'])
+        # ETL清洗
+        etl_func.run(list(datas))
+        # 继续查库
+        table_gov.commit()
+        n = table_gov.cursor.execute(sql)
+        datas = table_gov.cursor.fetchall()
+
+    print('ETL process is done!')
+    # 关闭数据库
+    table_gov.close()
+    etl_func.close()
+
+def Usage():
+    '''
+    使用说明
+    :return:
+    '''
+    print 'fetch.py usage:'
+    print '-h,--help: print help message.'
+    print '-v, --version: print script version'
+    print '-o, --outline: parse outline and save into table_outline'
+    print '-s, --seed: fetch seed url and insert into table_url'
+    print '-d, --detail: parse detail and insert into table_gov'
+    print '-e, --etl: clean data for table_gov and insert into table_detail'
+def Version():
+    '''
+    版本号
+    :return:
+    '''
+    print 'fetch.py 1.0.0'
+def main(argv):
+    '''
+    程序主入口
+    :param argv:终端参数
+    :return:
+    '''
+    try:
+        opts, args = getopt.getopt(argv[1:], 'hvo:', ['output=', 'foo=', 'fre='])
+    except getopt.GetoptError, err:
+        print str(err)
+        Usage()
+        sys.exit(2)
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            Usage()
+            sys.exit(1)
+        elif o in ('-v', '--version'):
+            Version()
+            sys.exit(0)
+        elif o in ('-o', '--outline'):
+            parseOutline()
+            sys.exit(0)
+        elif o in ('-s','--seed',):
+            parseSeedUrl()
+            sys.exit(0)
+        elif o in ('-d','--detail',):
+            parseDetail()
+        elif o in ('-e','--etl',):
+            etl()
+        else:
+            print 'unhandled option'
+            sys.exit(3)
+
+if __name__ == '__main__':
+    # main(sys.argv)
+    # etl()
+    # parseOutline()  # 抓取三级目录首页链接
+    # parseSeedUrl()    # 抓取所有产品链接
+    parseDetail()   # 抓取所有产品详情
+

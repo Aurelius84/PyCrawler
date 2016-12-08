03897d5a6c9705ed482bf02a95f040bf664fed4f
kevin <liujiezhang@bupt.edu.cn>
2016-12-08 22:02:26 +0800

    add try except

--- spiders/yqyb/detail.py
+++ spiders/yqyb/detail.py
@@ -113,47 +113,48 @@ def goodsDetail(detail_url):
     :return: 各个字段信息 dict
     '''
     goods_data = defaultdict()
-    # 详情页链接
-    goods_data['source_url'] = detail_url
-    # 解析html body必须是str类型
-    body = getHtmlByVPN(detail_url).decode('gbk').encode('utf8')
-    # print(body)
-    html = HtmlResponse(url=detail_url,body=body,encoding='utf8')
-    # 名称
-    goods_data['name'] = html.xpath('//*[@id="mod-detail-title"]/h1/text()').extract()[0]
-    # 价格
     try:
-        goods_data['price'] = html.xpath('//*[@id="mod-detail-price"]/div/table/tr[1]/td[2]/div/span[2]/text()').extract()[0]
-    except:
-        goods_data['price'] = html.xpath('//*[@id="mod-detail-price"]/div/table/tr[1]/td[2]/span[2]/text()').extract()[0]
-    # 参数名
-    names = html.selector.xpath('//*[@id="mod-detail-attributes"]/div[1]/table/tbody/tr/td[contains(@class,"de-feature")]/text()').extract()
-    params = html.selector.xpath('//*[@id="mod-detail-attributes"]/div[1]/table/tbody/tr/td[contains(@class,"de-value")]/text()').extract()
-    type = ''
-    if names and len(names) == len(params):
-        type = params[names.index(u'型号')]
-    # 型号
-    goods_data['type'] = type
-    # 详情
-    goods_data['detail'] = handleTable(names, params)
-    # 图片
-    pics = []
-    for pic in html.selector.xpath('//*[@id="dt-tab"]/div/ul/li/@data-imgs').extract():
-        # 去除图片尺寸,方法图片
-        # print(pic)
-        pics.append(json.loads(pic)['original'])
+        # 详情页链接
+        goods_data['source_url'] = detail_url
+        # 解析html body必须是str类型
+        body = getHtmlByVPN(detail_url).decode('gbk').encode('utf8')
+        # print(body)
+        html = HtmlResponse(url=detail_url,body=body,encoding='utf8')
+        # 名称
+        goods_data['name'] = html.xpath('//*[@id="mod-detail-title"]/h1/text()').extract()[0]
+        # 价格
+        try:
+            goods_data['price'] = html.xpath('//*[@id="mod-detail-price"]/div/table/tr[1]/td[2]/div/span[2]/text()').extract()[0]
+        except:
+            goods_data['price'] = html.xpath('//*[@id="mod-detail-price"]/div/table/tr[1]/td[2]/span[2]/text()').extract()[0]
+        # 参数名
+        names = html.selector.xpath('//*[@id="mod-detail-attributes"]/div[1]/table/tbody/tr/td[contains(@class,"de-feature")]/text()').extract()
+        params = html.selector.xpath('//*[@id="mod-detail-attributes"]/div[1]/table/tbody/tr/td[contains(@class,"de-value")]/text()').extract()
+        type = ''
+        if names and len(names) == len(params):
+            type = params[names.index(u'型号')]
+        # 型号
+        goods_data['type'] = type
+        # 详情
+        goods_data['detail'] = handleTable(names, params)
+        # 图片
+        pics = []
+        for pic in html.selector.xpath('//*[@id="dt-tab"]/div/ul/li/@data-imgs').extract():
+            # 去除图片尺寸,方法图片
+            # print(pic)
+            pics.append(json.loads(pic)['original'])
 
-    goods_data['pics'] = '|'.join(pics)
-    try:
-        goods_data['storage'] = html.selector.xpath('//*[@id="mod-detail-bd"]/div[2]/div[11]/div/div/div/div[1]/div[2]/table/tr[1]/td[3]/span/em[1]/text()').extract()[0]
-    except:
-        goods_data['storage'] = re.findall(r'\d+', html.xpath('//*[@id="mod-detail-bd"]/div[2]/div[12]/div/div/div[1]/div[2]/span/text()').extract()[0])[0]
+        goods_data['pics'] = '|'.join(pics)
+        try:
+            goods_data['storage'] = html.selector.xpath('//*[@id="mod-detail-bd"]/div[2]/div[11]/div/div/div/div[1]/div[2]/table/tr[1]/td[3]/span/em[1]/text()').extract()[0]
+        except:
+            goods_data['storage'] = re.findall(r'\d+', html.xpath('//*[@id="mod-detail-bd"]/div[2]/div[12]/div/div/div[1]/div[2]/span/text()').extract()[0])[0]
 
-    goods_data['lack_period'] = ''
-    goods_data['created'] = int(time.time())
-    goods_data['updated'] = int(time.time())
-    # except Exception,e:
-    #     print(Exception,e)
+        goods_data['lack_period'] = ''
+        goods_data['created'] = int(time.time())
+        goods_data['updated'] = int(time.time())
+    except Exception,e:
+         print(Exception,e)
 
     # print(goods_data['detail'])
     return goods_data

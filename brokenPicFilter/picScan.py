c68a395031ab1631f8e7d4ef023e7a8e6f418719
zoomadolf <zyforward@gmail.com>
2016-12-14 11:19:28 +0800

    Remote test

--- brokenPicFilter/picScan.py
+++ brokenPicFilter/picScan.py
@@ -1,32 +1,68 @@
 # -*- coding: utf-8 -*-
 
 from PIL import Image
+import os
 #from PIL import PixcelAcess
 
-def scanPic():
-	im = Image.open(u'1.jpg')
+def scanPic(filename):
+	im = Image.open(filename)
 	backcolor = im.getpixel((0,0))
-	print 'backcolor :' + str(backcolor)
+	#print 'backcolor :' + str(backcolor)
+	#print im.size
+	'''
 	for k in xrange(im.size[0]):
 		for i in xrange(im.size[1]):
-			if k > 1000:
+			if i > 200:
 				im.putpixel((k, i),(255, 0, 0))
+
+	im.save('2.jpg')
+	'''
+	# 坏图标记
 	brokenpic = False
-	frontcolor =(0,0,0)
 	masscolor = (0,0,0)
+	# 色块的大小
 	masslines = 0
-	for k in xrange(im.size[0]):
-		if im.getpixel((k,i)) != backcolor:
-			masscolor = im.getpixel((k,i))
+	for k in xrange(im.size[1]):
+		if im.getpixel((0, k)) != backcolor:
+			masscolor = im.getpixel((0, k))
+			#print masscolor
 		else:
+			#print k
 			continue
-		for i in xrange(im.size[1]):
-			if im.getpixel((k,i)) != masscolor:
-				continue
-		masslines += 1
+		for i in xrange(im.size[0]):
+			if im.getpixel((i, k)) != masscolor:
+				break
+			elif i == im.size[0]-1:
+				#print 'massline:{0},{1}'.format(i,k)
+				masslines += 1
+
 		if masslines > 50:
 			brokenpic = True
-			print 'Broken pic detected.'
+			filename = filename.split(u'\\')[-1]
+			print 'broken picture detected:  {0} '.format(filename.encode('utf-8'))
+			#print 'deleting...'
+			#deletefile(filename)
 			break
+	#print str(masslines) +':' + str(brokenpic)
+
+def explore(dir):
+	for root,dir,files in os.walk(dir):
+		for file in files:
+			filename = os.path.join(root,file)
+			#print filename
+			scanPic(filename)
+
+def deletefile(filename):
+	'''
+	:param filename: 绝对地址
+	:return:
+	'''
+	#print type(filename)
+	os.remove(filename)
+	print filename + ' deleted'
 if __name__ == '__main__':
-	scanPic()
+	#filename = u'D:\\git\\PyCrawler\\brokenPicFilter\\慧聪图片数据\\3.jpg'
+	dir = u'D:\\git\\PyCrawler\\brokenPicFilter\\慧聪图片数据'
+	explore(dir)
+	#scanPic(filename)
+	#deletefile(filename)
